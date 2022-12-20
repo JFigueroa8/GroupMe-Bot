94d0ace2aa0c0f61bd22b70a890f9b7ab8130fe3
Julmar <Julmarfigueroa@gmail.com>
2022-12-20 12:00:45 -0500

    Added the ability to search for and post gifs

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import requests
+import random
 from flask import Flask, request, jsonify
-from config import access_token, bot_id
+from config import access_token, bot_id, giphy_api_key
 
 app = Flask(__name__)
 
-url = 'https://api.groupme.com/v3/bots/post'
-
-@app.route('/')
+groupme_url = 'https://api.groupme.com/v3/bots/post'
+giphy_url = f'http://api.giphy.com/v1/gifs/search?api_key={giphy_api_key}&q='
 
 @app.route('/callback', methods=['POST', 'GET'])
 def callback():
@@ -18,11 +18,15 @@ def callback():
   if sender_type != "user":
     return jsonify({'status': 'OK'}), 200
 
-  if 'giphy' in text:
-    text = text.replace('giphy ', '').capitalize()
+  if '$giphy' in text:
+    text = text.replace('$giphy ', '').lower()
+    giphy_data = requests.get(giphy_url + text).json()
+    results_length = len(giphy_data['data'])
+    random_index = random.randint(0, results_length - 1)
+    gif = giphy_data['data'][random_index]['images']['original']['url']
     payload = {
       'bot_id': bot_id,
-      'text': f'You said: {text}',
+      'text': gif,
     }
   else:
     return jsonify({'status': 'OK'}), 200
@@ -32,9 +36,8 @@ def callback():
     'X-Access-Token': access_token
   }
 
-  response = requests.post(url, json=payload, headers=headers)
+  response = requests.post(groupme_url, json=payload, headers=headers)
   
-
   return jsonify({'status': 'OK'}), 200
 
 if __name__ == '__main__':
