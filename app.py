1b9b9461de96511c515c10d24d1a6c6ef303e8b1
Julmar <Julmarfigueroa@gmail.com>
2022-12-28 15:18:40 -0500

    Moved the urls to the urls.py file

--- app.py
+++ app.py
@@ -4,17 +4,9 @@ import time
 from datetime import date, timedelta
 from flask import Flask, request, jsonify
 from config import access_token, bot_id, giphy_api_key, dad_api
+from urls import groupme_url, giphy_url, zenquotes_url, dad_jokes_url, chuck_norris_url, nba_url, nfl_url
 
 app = Flask(__name__)
-
-groupme_url = 'https://api.groupme.com/v3/bots/post'
-giphy_url = f'http://api.giphy.com/v1/gifs/search?api_key='
-zenquotes_url = 'https://zenquotes.io/api/random'
-dad_jokes_url = 'https://icanhazdadjoke.com/slack'
-chuck_norris_url = 'https://api.chucknorris.io/jokes/random'
-nba_url = 'https://site.web.api.espn.com/apis/v2/scoreboard/header?sport=basketball&league=nba'
-nfl_url = 'https://site.web.api.espn.com/apis/v2/scoreboard/header?sport=football&league=nfl'
-
 
 @app.route('/callback', methods=['POST', 'GET'])
 def callback():
