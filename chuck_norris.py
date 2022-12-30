d6883c0482bcd54ed4c05be1a876ed55f73dbb73
Julmar <Julmarfigueroa@gmail.com>
2022-12-30 15:01:48 -0500

    Cleaned up and commented

---
+++ chuck_norris.py
@@ -0,0 +1,24 @@
+import requests
+from config import access_token, bot_id
+from urls import groupme_url, chuck_norris_url
+
+def chuck_joke():
+  #Get the data from the Chuck Norris API
+  chuck_norris_data = requests.get(chuck_norris_url).json()
+
+  #Get the joke
+  chuck_norris_joke = chuck_norris_data['value']
+
+  #Set the headers and payload
+  headers = {
+    'Content-Type': 'application/json',
+    'X-Access-Token': access_token,
+  }
+
+  payload = {
+    'bot_id': bot_id,
+    'text': chuck_norris_joke,
+  }
+
+  #Send the joke to GroupMe
+  response = requests.post(groupme_url, json=payload, headers=headers)
