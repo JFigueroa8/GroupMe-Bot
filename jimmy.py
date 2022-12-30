4c9c425fc91de112b0de2141ac732251bdbe4be5
Julmar <Julmarfigueroa@gmail.com>
2022-12-30 15:40:08 -0500

    Cleaned up and commented

--- jimmy.py
+++ jimmy.py
@@ -4,23 +4,29 @@ from config import access_token, bot_id
 from urls import groupme_url
 
 def jimmy_images():
-  # directory for images
+  # Directory for images
   directory = 'jimmy_images'
 
-  # iterate over files in that directory
+  # Iterate over files in that directory
   for filename in os.listdir(directory):
 
-    # get the size of the image in bytes
+    # Get the size of the image in bytes
     image_size = os.stat('jimmy_images/' + filename).st_size
     data = open('jimmy_images/' + filename, 'rb').read()
-    headers={
+
+    # Set the headers for the first post request
+    headers = {
       'Content-Type': 'image/jpeg',
       'Content-Length': str(image_size),
       'X-Access-Token': access_token}
 
+    # Send the image to GroupMe image service
     groupme_response = requests.post(url='https://image.groupme.com/pictures', data=data, headers=headers)
+
+    # Get the GroupMe image URL created by the GroupMe image service from the response
     image_url = groupme_response.json()['payload']['picture_url']
 
+    # Set the headers for the second post request, which will send the image to GroupMe
     headers = {
     'Content-Type': 'image/jpeg',
     'X-Access-Token': access_token,
@@ -35,4 +41,6 @@ def jimmy_images():
         }
       ],
     }
+
+    # Send the image to GroupMe
     response = requests.post(groupme_url, json=payload, headers=headers)
