3eda3cfb4bff83b0d8209837b59f89ac8b17b755
Julmar <Julmarfigueroa@gmail.com>
2022-12-30 00:20:02 -0500

    Refactored into app.py

--- allen.py
+++ allen.py
@@ -1,41 +1,20 @@
 import requests
-from flask import Flask, request, jsonify
 from config import access_token, bot_id
 from urls import groupme_url
 
-app = Flask(__name__)
-
-@app.route('/callback', methods=['POST', 'GET'])
-def callback():
-  data = request.get_json()
-  text = data['text']
-  sender_type = data['sender_type']
-
-  if sender_type != "user":
-    return jsonify({'status': 'OK'}), 200
-
-  if '$allen' in text:
-    headers = {
+def allen_dance():
+  headers = {
     'Content-Type': 'application/json',
     'X-Access-Token': access_token,
-    }
+  }
 
-    payload = {
-      'bot_id': bot_id,
-      'attachments': [
-        {
-          "type":"image",
-          "url":"https://i.groupme.com/330x200.gif.9de6ba82c66a4a7c86b4f6be307b731e"
-        }
-      ],
-    }
-    response = requests.post(groupme_url, json=payload, headers=headers)
-    
-  else:
-    return jsonify({'status': 'OK'}), 200
-
-  return jsonify({'status': 'OK'}), 200
-
-if __name__ == '__main__':
-  app.debug = True
-  app.run(port=3000)
+  payload = {
+    'bot_id': bot_id,
+    'attachments': [
+      {
+        "type":"image",
+        "url":"https://i.groupme.com/330x200.gif.9de6ba82c66a4a7c86b4f6be307b731e"
+      }
+    ],
+  }
+  response = requests.post(groupme_url, json=payload, headers=headers)
--- jimmy.py
+++ jimmy.py
@@ -1,62 +1,41 @@
 import requests
 import os
-from flask import Flask, request, jsonify
 from config import access_token, bot_id
 from urls import groupme_url
 
-app = Flask(__name__)
+def jimmy_images():
+  image_url_list = []
 
-@app.route('/callback', methods=['POST', 'GET'])
-def callback():
-  data = request.get_json()
-  text = data['text']
-  sender_type = data['sender_type']
+  # directory for images
+  directory = 'jimmy_images'
 
-  if sender_type != "user":
-    return jsonify({'status': 'OK'}), 200
+  # iterate over files in that directory
+  for filename in os.listdir(directory):
 
-  if '$jimmy' in text:
-    image_url_list = []
-
-    # directory for images
-    directory = 'jimmy_images'
-
-    # iterate over files in that directory
-    for filename in os.listdir(directory):
-
-      # get the size of the image in bytes
-      image_size = os.stat('jimmy_images/' + filename).st_size
-      data = open('jimmy_images/' + filename, 'rb').read()
-      headers={
-        'Content-Type': 'image/jpeg',
-        'Content-Length': str(image_size),
-        'X-Access-Token': access_token}
-
-      groupme_response = requests.post(url='https://image.groupme.com/pictures', data=data, headers=headers)
-      image_url = groupme_response.json()['payload']['picture_url']
-      image_url_list.append(image_url)
-
-      headers = {
+    # get the size of the image in bytes
+    image_size = os.stat('jimmy_images/' + filename).st_size
+    data = open('jimmy_images/' + filename, 'rb').read()
+    headers={
       'Content-Type': 'image/jpeg',
-      'X-Access-Token': access_token,
-      }
+      'Content-Length': str(image_size),
+      'X-Access-Token': access_token}
 
-      payload = {
-        'bot_id': bot_id,
-        'attachments': [
-          {
-          'type': 'image',
-          'url': image_url,
-          }
-        ],
-      }
-      response = requests.post(groupme_url, json=payload, headers=headers)
-    
-  else:
-    return jsonify({'status': 'OK'}), 200
+    groupme_response = requests.post(url='https://image.groupme.com/pictures', data=data, headers=headers)
+    image_url = groupme_response.json()['payload']['picture_url']
+    image_url_list.append(image_url)
 
-  return jsonify({'status': 'OK'}), 200
+    headers = {
+    'Content-Type': 'image/jpeg',
+    'X-Access-Token': access_token,
+    }
 
-if __name__ == '__main__':
-  app.debug = True
-  app.run(port=3000)
+    payload = {
+      'bot_id': bot_id,
+      'attachments': [
+        {
+        'type': 'image',
+        'url': image_url,
+        }
+      ],
+    }
+    response = requests.post(groupme_url, json=payload, headers=headers)
