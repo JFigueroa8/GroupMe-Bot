89acb1f2238dbe6d85efed78bd8fd4ee9f6c828b
Julmar <Julmarfigueroa@gmail.com>
2022-12-31 21:56:23 -0500

    Created a file that displays some of the available commands

---
+++ commands.py
@@ -0,0 +1,30 @@
+import requests
+from config import access_token, bot_id
+from urls import groupme_url
+
+def command_list():
+  # Set the headers and payload
+  headers = {
+    'Content-Type': 'application/json',
+    'X-Access-Token': access_token,
+  }
+
+  payload = {
+    'bot_id': bot_id,
+    'text': """$giphy <search term> - returns a random gif from Giphy
+
+$quote - returns a random quote
+
+$dad joke - returns a random dad joke for Cory, Steve and Jay
+
+$chuck - returns a random Chuck Norris joke
+
+$nba yesterday - returns yesterday's NBA scores
+
+$nba - returns today's NBA scores
+
+$nfl - returns NFL scores"""
+  }
+
+  # Send the image to GroupMe
+  response = requests.post(groupme_url, json=payload, headers=headers)
