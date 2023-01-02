0b693b07856da6d740bab24a85571015dc5c396d
Julmar <Julmarfigueroa@gmail.com>
2023-01-02 10:30:20 -0500

    Completed but need to clean up

--- snap_restart.py
+++ snap_restart.py
@@ -40,33 +40,31 @@ def grab_card_ids(character):
       variant_card_links.extend(href_values)
 
   for url in variant_card_links:
-    
+
     # Extract the text from the sibling div tags then add it to the dictionary
     variant_id = url.split("/")
     variant_id = variant_id[-2]
     
     card_ids.append(variant_id)
-
+  
   return card_ids
 
-def grab_every_description(list_of_ids, character_name):
+def grab_every_description(card_ids, character_name):
   character_description_list = []
   variant_image_urls_list = grab_image_urls(character_name)
-  print('Length of variant_image_urls_list: ', len(variant_image_urls_list))
-  print('Length of list_of_ids: ', len(list_of_ids))
+  url = f"https://marvelsnapzone.com/cards/{id}"
 
-  for count, id in enumerate(list_of_ids):
-    # print(list_of_ids)
+  for count, id in enumerate(card_ids):
     character_description = {}
     if count == 0:
       # Make a request to the website and retrieve the HTML
-      html = requests.get(f"https://marvelsnapzone.com/cards/{id}").text
+      html = requests.get(url).text
 
       # Use Beautiful Soup to parse the HTML
       soup = BeautifulSoup(html, "html.parser")
 
       # Find all the a tags with the href attribute value that contains the character name
-      a_tags = soup.find_all(href=re.compile(f"https://marvelsnapzone.com/cards/{id}"))
+      a_tags = soup.find_all(href=re.compile(url))
       
       character_description['name'] = a_tags[1]["data-name"]
       if a_tags[1]["data-ability"] == "":
@@ -96,8 +94,9 @@ def grab_every_description(list_of_ids, character_name):
       character_description_list.append(character_description)
 
     else:
+      variant_url = f"https://marvelsnapzone.com/variants/{id}"
       # Make a request to the website and retrieve the HTML
-      html = requests.get(f"https://marvelsnapzone.com/variants/{id}").text
+      html = requests.get(variant_url).text
 
       # Use Beautiful Soup to parse the HTML
       soup = BeautifulSoup(html, "html.parser")
@@ -114,20 +113,22 @@ def grab_every_description(list_of_ids, character_name):
       # Get the text from the sibling div tag
       card_status_text = status_sibling_div_tags[0].text
 
-      # Check if the div_tag_rarity is None, if it is then the card is an unreleased card and we'll assign it the card status text which should be "Unreleased"
-      if div_tag_rarity == None:
-        div_tag_rarity = card_status_text
-        # print(id)
-        character_description = {'card_id': id, 'status': card_status_text, 'rarity': div_tag_rarity, 'image_url': variant_image_urls_list[count-1]}
-        character_description_list.append(character_description)
+      if variant_image_urls_list[count-1] == 'error':
+        continue
       else:
-        # Find the sibling div tags and extract the text from the sibling div tags then add it to the dictionary
-        rarity_sibling_div_tags = div_tag_rarity.find_next_siblings("div")
-        card_rarity_text = rarity_sibling_div_tags[0].text
-        character_description = {'card_id': id, 'status': card_status_text, 'rarity': card_rarity_text, 'image_url': variant_image_urls_list[count-1]}
-        # Variant ID, Status, Rarity
-        character_description_list.append(character_description)
-  # print(character_description_list)
+        # Check if the div_tag_rarity is None, if it is then the card is an unreleased card and we'll assign it the card status text which should be "Unreleased"
+        if div_tag_rarity == None:
+          div_tag_rarity = card_status_text
+          character_description = {'card_id': id, 'status': card_status_text, 'rarity': div_tag_rarity, 'image_url': variant_image_urls_list[count-1]}
+          character_description_list.append(character_description)
+        else:
+          # Find the sibling div tags and extract the text from the sibling div tags then add it to the dictionary
+          rarity_sibling_div_tags = div_tag_rarity.find_next_siblings("div")
+          card_rarity_text = rarity_sibling_div_tags[0].text
+          character_description = {'card_id': id, 'status': card_status_text, 'rarity': card_rarity_text, 'image_url': variant_image_urls_list[count-1]}
+          # Variant ID, Status, Rarity
+          character_description_list.append(character_description)
+  
   return character_description_list
 
 def grab_image_urls(character):
@@ -143,7 +144,7 @@ def grab_image_urls(character):
 
   # Find all the <a> tags with the class "variant"
   links = soup.find_all('a', class_='variant')
-
+  
   # Iterate over the links and find the images with the attribute "data-src"
   image_links = []
 
@@ -152,11 +153,11 @@ def grab_image_urls(character):
 
     for image in images:
       response = requests.get(image['data-src'])
-      print(image)
+    
       if response.status_code == 200:
         image_links.append(image['data-src'])
       else:
-        print(f'Error for image {image} - {image["data-src"]}')
+        image_links.append('error')
   
   return image_links
 
@@ -175,8 +176,6 @@ def grab_card_images(card_description_list):
       
     # exporting the image
     rgb_image.save(f"images/{count}.jpg")
-
-
 
 app = Flask(__name__)
 
@@ -201,13 +200,7 @@ def callback():
       character_name = character_name.replace(' ', '-')
 
     card_ids = grab_card_ids(character_name)
-    urls = grab_image_urls(character_name)
 
-    # ******** WORKAROUND FOR HULK *******************
-    if character_name == 'hulk':
-      card_ids.pop()
-      print(f'card_ids length - {len(card_ids)}')
-      print(f'urls length - {len(urls)}')
     list_of_descriptions = grab_every_description(card_ids, character_name)
     grab_card_images(list_of_descriptions)
 
@@ -234,10 +227,10 @@ def callback():
       image_url_list.append(image_url)
       
     for count, card in enumerate(list_of_descriptions):
-      # print(card)
       card_status = card['status']
       card['groupme_image_url'] = image_url_list[count]
       card_groupme_url = card['groupme_image_url']
+      # print(card)
       card_id = card['card_id']
 
       if count == 0:
